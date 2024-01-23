77550d4669a254279ece35bf8c853b4ed6544aa6
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-01-23 16:15:13 +0800

    Learn variables in PHP.

---
+++ 02-variables/variables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $characterName = "Emman";
+        $characterAge = 35;
+
+        echo "hi $characterName";
+    ?>
+</body>
+</html>
