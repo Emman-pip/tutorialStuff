db621bcdb4859bd48e86118139214b6519c798ec
emman-pip <parrenoemmanuel756@gmail.com>
2024-01-25 17:04:47 +0800

    More items to arr.

--- 05-arrays/arrays.php
+++ 05-arrays/arrays.php
@@ -1,6 +1,11 @@
 <?php
     $arrays = "Arrays";
-    echo "<h1> $arrays <h1?";
+    // echo "<h1> $arrays <h1?";
+    // key value format
     $arr1 = array("a"=>"hahaha", "b"=>"gfsdf");
-    var_dump($arr1["a"]);
+    var_dump($arr1);
+    // ommit keys
+    $arr2 = array(1,2,3,4,5,6,7,8,9);
+    var_dump($arr2);
+    echo count($arr2);
 ?>
