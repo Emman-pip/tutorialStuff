f6256ead9e4ff44704f642b64302b4bfc12fbdb0
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-01-26 18:07:06 +0800

    Learn include.

---
+++ 12-include/include.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $title = 21321312313;
+        include "backend.php";
+        include "header.html";
+    ?>
+</body>
+</html>
